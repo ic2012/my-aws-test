0e23f727287ec03da85157fa61dd0aeacaac1004
ic2012 <87852170+ic2012@users.noreply.github.com>
2023-01-16 16:27:44 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,18 @@
-provider "aws" {
-  region = "us-west-2"
-}
-
-variable "unique_string" {
-  default = "akunamatata007"
-  type = string
-}
-
 resource "aws_s3_bucket" "my-bucket" {
   bucket = "my-bucket-name-${var.unique_string}"
-  acl    = "public-read"
+}
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+resource "aws_s3_bucket_server_side_encryption_configuration" "my-bucket" {
+  bucket = aws_s3_bucket.my-bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
+
+resource "aws_s3_bucket_acl" "my-bucket" {
+  bucket = aws_s3_bucket.my-bucket.id
+  acl    = "private"
+}
+
