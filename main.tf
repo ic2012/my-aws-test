ee402d1dbfdd2441c7990a235bf34ae6ff7fcc09
ic2012 <87852170+ic2012@users.noreply.github.com>
2023-01-16 15:47:25 +0100

    tf main update

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 # Variables
-variable "s3_bucket_name" {
+variable "unique_string" {
   type = string
 }
 
 # Resources
-resource "aws_s3_bucket" "example" {
-  bucket = var.s3_bucket_name
+resource "aws_s3_bucket" "my-bucket" {
+  bucket = bucket = "my-bucket-${var.unique_string}"
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
@@ -16,6 +16,6 @@ resource "aws_s3_bucket" "example" {
 }
 
 # Output
-output "s3_bucket_name" {
-  value = aws_s3_bucket.example.bucket
+output "unique_string" {
+  value = aws_s3_bucket.my-bucket.bucket
 }
